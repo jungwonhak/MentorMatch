c11f3afdc04f1d3cce9c196eda85f0b8a66c0695
Nicole D <nicole.dun@gmail.com>
2014-04-26 15:56:40 -0400

    changes before big code change

---
+++ src/com/codepath/mentormatch/models/Task.java
@@ -0,0 +1,53 @@
+package com.codepath.mentormatch.models;
+
+import java.util.Date;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("Task")
+public class Task extends ParseObject {
+	public static final String RELATIONSHIP_KEY = "relationshipId";
+	public static final String DESCRIPTION_KEY = "description";
+	public static final String DUE_DATE_KEY = "dueDate";
+	public static final String MENTEE_RATING_KEY = "menteeRating";
+	public static final String STATUS_KEY = "status";
+
+	public Task() {
+		super();
+	}
+	
+	public String getStatus() {
+		return getString(STATUS_KEY);
+	}
+	
+	public void setStatus(String value) {
+		put(STATUS_KEY, value);
+	}
+	
+	public String getDescription() {
+		return getString(DESCRIPTION_KEY);
+	}	
+	
+	public void setDescription(String comment) {
+		put(DESCRIPTION_KEY, comment);
+	}
+	
+	public Date getDueDate() {
+		return getDate(DUE_DATE_KEY);
+	}
+	
+	public void setDueDate(Date date) {
+		put(DUE_DATE_KEY, date);
+	}
+	
+	public void setMatchRelationshipId(MatchRelationship requestId) {
+		put(RELATIONSHIP_KEY, requestId);
+	}
+	
+	public MatchRelationship getMatchRelationshipId() {
+		return (MatchRelationship) getParseObject(RELATIONSHIP_KEY);
+	}
+
+	
+}
